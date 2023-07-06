e094047367fcacb558cb2da4b4dd82f0b926a2df
Swaraj <67910599+swarajtwok@users.noreply.github.com>
2023-07-05 19:01:04 -0500

    Create API.py

---
+++ Assignments/A08/API.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import csv
+import pandas as pd
+
+description = """🚀
+## 4883 Software Tools
+### Where awesomeness happens
+### Swaraj Chirumamilla
+"""
+
+class DataReader:
+    def __init__(self, csv_file):
+        self.data = None
+        self.load_data(csv_file)
+
+    def load_data(self, csv_file):
+        self.data = pd.read_csv(csv_file)
+
+    def get_attribute(self, attribute):
+        return json.dumps(self.data[attribute].unique().tolist())
+
+    def get_unique_countries(self):
+        unique_countries = self.data["Country"].unique().tolist()
+        return unique_countries
+
+# Instantiate the DataReader class with the CSV file path
+mydb = DataReader(r'C:\Users\swara\OneDrive\Desktop\assignemnet\data.csv')
+
+# print(mydb.data.head())
+# print(mydb.data.shape[0])
+
+# Initialize the FastAPI app with the provided description
+app = FastAPI(description=description)
+
+@app.get("/")
+async def docs_redirect():
+    """Api's base route that displays the information created above in the ApiInfo section."""
+    return RedirectResponse(url="/docs")
+
+@app.get("/countries/")
+async def countries():
+    unique_countries = mydb.get_unique_countries()
+    return {"countries": unique_countries}
+
+
+
+
+
+if __name__ == "__main__":
+        uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="debug", reload=True) #host="127.0.0.1"
