83ba99ea1eaa0f7689c8409bc0d4339bffd3a4ba
Swaraj <67910599+swarajtwok@users.noreply.github.com>
2023-07-06 01:09:13 -0500

    Update API.py

--- Assignments/A08/API.py
+++ Assignments/A08/API.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import csv
 import pandas as pd
+from fastapi import FastAPI, Query
 
 description = """🚀
 ## 4883 Software Tools
@@ -17,7 +18,11 @@ class DataReader:
         self.load_data(csv_file)
 
     def load_data(self, csv_file):
-        self.data = pd.read_csv(csv_file)
+        try:
+            self.data = pd.read_csv(csv_file)
+        except FileNotFoundError:
+            print(f"Error: File {csv_file} not found.")
+            self.data = None
 
     def get_attribute(self, attribute):
         return json.dumps(self.data[attribute].unique().tolist())
@@ -29,6 +34,7 @@ class DataReader:
     def get_available_regions(self):
         available_regions = self.data["WHO_region"].unique().tolist()
         return available_regions
+
 
 # Instantiate the DataReader class with the CSV file path
 mydb = DataReader(r'C:\Users\swara\OneDrive\Desktop\assignemnet\data.csv')
@@ -53,6 +59,40 @@ async def countries():
 def regions():
     available_regions = mydb.get_available_regions()
     return {"regions": available_regions}
+
+@app.get("/deaths/")
+async def deaths(country:str = None,year:int = None,region:str = None):
+    """
+    This method will return a total death count or can be filtered by country and year.
+    - Params:
+      - country (str) : A country name
+      - year (int) : A 4 digit year
+      - region (str) : A region name
+    - **Returns:**
+      - (int) : The total sum of deaths based on filters (if any)
+    """
+    response=int(mydb.data['New_deaths'].sum())
+    message=f"Total number of entire deaths {response}"
+    if year!=None:
+        # response=int(mydb.data.loc[pd.to_datetime(mydb.data['Date_reported']).dt.year == int(year), 'New_deaths'].sum())
+        if country!=None:
+            response=int(mydb.data.loc[(pd.to_datetime(mydb.data['Date_reported']).dt.year == int(year)) & (mydb.data['Country'] == country), 'New_deaths'].sum())
+            message=f"Total number of deaths in {year} for the country of {country} is {response}"
+        if region!=None:
+            response=int(mydb.data.loc[(pd.to_datetime(mydb.data['Date_reported']).dt.year == int(year)) & (mydb.data['WHO_region'] == region), 'New_deaths'].sum())
+            message=f"Total number of deaths in {year} for the WHO region in {region} is {response}"
+        else:
+            response=int(mydb.data.loc[pd.to_datetime(mydb.data['Date_reported']).dt.year == int(year), 'New_deaths'].sum())
+            message=f"Total number of deaths in {year} is {response}"
+    else:
+        if country!=None:
+            response=int(mydb.data[mydb.data['Country'] == country]['New_deaths'].sum())
+            message=f"Total number of deaths in the country of {country} is {response}"
+        if region!=None:
+            response=int(mydb.data[mydb.data['WHO_region'] == region]['New_deaths'].sum())
+            message=f"Total number of deaths in the WHO region of {region} is {response}"
+
+    return {"data": response, "success": True, "message": message}
 
 
 
